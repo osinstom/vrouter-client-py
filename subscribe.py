3e2a014e1db8c08c9eabe13463401692f2944a7a
Tomek Osiński <osinstom@gmail.com>
2017-08-30 18:04:23 +0200

    sending raw XMPP packets

--- subscribe.py
+++ subscribe.py
@@ -4,13 +4,15 @@ import xml.etree.ElementTree
 
 xmpp = ClientXMPP('somejid@example.com', 'use_getpass')
 
-pubsub_data = xml.etree.ElementTree.parse('testdata/pubsub.xml').getroot()
+# pubsub_data = xml.etree.ElementTree.parse('testdata/pubsub.xml').getroot()
 # xmlstr = xml.etree.ElementTree.tostring(xml.etree.ElementTree.tostring(pubsub_data, encoding='utf8', method='xml'))
 
-
+str = open('testdata/pubsub_pub.xml', 'r').read()
+print str
 
 if xmpp.connect(('127.0.0.1', 5269)):
-    xmpp.send_xml(pubsub_data)
+    # xmpp.send_presence()
+    xmpp.send_raw(str, now=True)
     # xmpp.process(block=True)
 
 
