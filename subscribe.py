e25f05e0f0a42cc6dd64b78c1e5289ded7b5e969
Tomek Osiński <osinstom@gmail.com>
2017-09-01 17:57:41 +0200

    block connection after sending XML

--- subscribe.py
+++ subscribe.py
@@ -2,19 +2,23 @@ from sleekxmpp import ClientXMPP
 import xml.etree.ElementTree
 
 
-xmpp = ClientXMPP('somejid@example.com', 'use_getpass')
+xmpp = ClientXMPP('somejid@example.com', 'use_getpass',  end_session_on_disconnect = False)
 
 # pubsub_data = xml.etree.ElementTree.parse('testdata/pubsub.xml').getroot()
 # xmlstr = xml.etree.ElementTree.tostring(xml.etree.ElementTree.tostring(pubsub_data, encoding='utf8', method='xml'))
 
-str = open('testdata/pubsub_pub.xml', 'r').read()
+str = open('testdata/pubsub.xml', 'r').read()
 print str
 
 if xmpp.connect(('127.0.0.1', 5269)):
+    print 'connected'
     # xmpp.send_presence()
     xmpp.send_raw(str, now=True)
+    # xmpp.send_raw(str, now=True)
+    while True:
+        'dds'
     # xmpp.process(block=True)
-
+    # xmpp.disconnect()
 
 
 
