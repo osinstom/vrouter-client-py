acd81dac61ae9e4a779dcf06728b25fb56f5dd30
Tomek Osiński <osinstom@gmail.com>
2017-09-08 15:40:12 +0200

    XMPP streaming impl

--- subscribe.py
+++ subscribe.py
@@ -1,8 +1,9 @@
-from sleekxmpp import ClientXMPP
+from sleekxmpp import ClientXMPP, XMLStream
+import time
 import xml.etree.ElementTree
 
 
-xmpp = ClientXMPP('somejid@example.com', 'use_getpass',  end_session_on_disconnect = False)
+xmpp = XMLStream(host='127.0.0.1', port=5269)
 
 # pubsub_data = xml.etree.ElementTree.parse('testdata/pubsub.xml').getroot()
 # xmlstr = xml.etree.ElementTree.tostring(xml.etree.ElementTree.tostring(pubsub_data, encoding='utf8', method='xml'))
@@ -10,10 +11,31 @@ xmpp = ClientXMPP('somejid@example.com', 'use_getpass',  end_session_on_disconne
 str = open('testdata/pubsub.xml', 'r').read()
 print str
 
-if xmpp.connect(('127.0.0.1', 5269)):
+if xmpp.connect(host='127.0.0.1', port=5269, use_tls=False):
+
+    ifrom = 'agent@vnsw.contrailsystems.com/other-peer'
+    ito = 'bgp.contrail.com'
+
+
+
+    xmpp.stream_header= "<stream:stream " \
+                        "from='{}' " \
+                        "to='{}' " \
+                        "version='1.0' " \
+                        "xml:lang='en' " \
+                        "xmlns='jabber:client' " \
+                        "xmlns:stream='http://etherx.jabber.org/streams'>".format(ifrom, ito)
+    xmpp.process(block=True)
     print 'connected'
+
     # xmpp.send_presence()
-    xmpp.send_raw(str, now=True)
+    # iq = xmpp.make_iq(id="req1", ifrom="agent@contrail.com", ito="controller@contrail.com")
+    # iq.send(now=True)
+    # time.sleep(1)
+    # iq.send(now=True)
+    # xmpp.send_raw(str, now=True)
+    # time.sleep(1)
+    # xmpp.send_raw(str, now=True)
     # xmpp.send_raw(str, now=True)
     while True:
         'dds'
